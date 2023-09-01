c8aa73cefa07ca2f0f9e2a3924ed8e4eb2aa452a
lxgrf <github@lxgrf.co.uk>
2023-09-01 23:08:32 +0100

    Move to chat completion

--- main.py
+++ main.py
@@ -6,14 +6,15 @@ import os
 
 openai_api = str(os.environ['openai'])
 discord_api = str(os.environ['discord'])
-model = "text-davinci-003"
+# model = "text-davinci-003"
+model = "gpt-3.5-turbo"
 
 bot = Client(intents=Intents.all())
 slash = SlashCommand(bot, sync_commands=True)
 
 url = 'https://api.openai.com/v1/completions'
 headers = {'content-type': 'application/json', "Authorization":f'Bearer {openai_api}'}
-footer = "/scene | Request your own scene prompt! Prompts are AI-generated, so feel free to change or ignore any detail. It's your scene!"
+footer = f"/scene | Request your own scene prompt! Prompts are AI-generated, so feel free to change or ignore any detail. It's your scene! Generated with {model}."
 
 @slash.slash(name="scene", description="Get a scene prompt! Describe the characters involved specifying any relevant detail.")
 async def scene(ctx: SlashContext, character1, character2, request=""):
@@ -23,11 +24,19 @@ async def scene(ctx: SlashContext, character1, character2, request=""):
     if request != "": 
         prompt += f" {request}."
         description += f"\n**Request**: `{request}`"
+    messages = [
+        {"role": "system", "content": "You are a D&D Dungeonmaster."},
+        {"role": "user", "content":prompt},
+    ]
     # payload = {"model":"text-davinci-003","prompt":prompt,"temperature":0.8,"max_tokens":150}
-    payload = {"model":model,"prompt":prompt,"temperature":0.8,"max_tokens":150}
+    payload = {
+        "model":model,
+        "messages":messages,
+        "temperature":0.8,
+        "max_tokens":150}
     payload = json.dumps(payload, indent = 4)
     r = requests.post(url=url, data=payload, headers=headers)
-    description += f"\n\n{r.json()['choices'][0]['text']}"
+    description += f"\n\n{r.json()['choices'][0]['message']['content']}"
     embed = Embed(title=f"Here is your scene prompt!", description=description)
     embed.set_footer(text=footer)
     await ctx.send(embed=embed)
@@ -40,11 +49,21 @@ async def solo(ctx: SlashContext, character, request=""):
     if request != "": 
         prompt += f" {request}."
         description += f"\n**Request**: `{request}`"
+    messages = [
+        {"role": "system", "content": "You are a D&D Dungeonmaster."},
+        {"role": "user", "content":prompt},
+        ]
     # payload = {"model":"text-davinci-003","prompt":prompt,"temperature":0.8,"max_tokens":150}
-    payload = {"model":model,"prompt":prompt,"temperature":0.8,"max_tokens":150}
+    payload = {
+        "model":model,
+        "messages":messages,
+        "temperature":0.8,
+        "max_tokens":150}
+    # payload = {"model":"text-davinci-003","prompt":prompt,"temperature":0.8,"max_tokens":150}
+    # payload = {"model":model,"prompt":prompt,"temperature":0.8,"max_tokens":150}
     payload = json.dumps(payload, indent = 4)
     r = requests.post(url=url, data=payload, headers=headers)
-    description += f"\n\n{r.json()['choices'][0]['text']}"
+    description += f"\n\n{r.json()['choices'][0]['message']['content']}"
     embed = Embed(title=f"Here is your solo prompt!", description=description)
     embed.set_footer(text=footer)
     await ctx.send(embed=embed)
