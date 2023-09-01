aa0656456320728ea66fd3498379c6016b9b87db
lxgrf <github@lxgrf.co.uk>
2023-09-01 23:22:58 +0100

    Had the wrong API url

--- main.py
+++ main.py
@@ -12,7 +12,8 @@ model = "gpt-3.5-turbo"
 bot = Client(intents=Intents.all())
 slash = SlashCommand(bot, sync_commands=True)
 
-url = 'https://api.openai.com/v1/completions'
+# url = 'https://api.openai.com/v1/completions'
+url = 'https://api.openai.com/v1/chat/completions'
 headers = {'content-type': 'application/json', "Authorization":f'Bearer {openai_api}'}
 footer = f"/scene | Request your own scene prompt! Prompts are AI-generated, so feel free to change or ignore any detail. It's your scene! Generated with {model}."
 
@@ -33,7 +34,7 @@ async def scene(ctx: SlashContext, character1, character2, request=""):
         "model":model,
         "messages":messages,
         "temperature":0.8,
-        "max_tokens":150}
+        "max_tokens":200}
     payload = json.dumps(payload, indent = 4)
     r = requests.post(url=url, data=payload, headers=headers)
     description += f"\n\n{r.json()['choices'][0]['message']['content']}"
@@ -58,7 +59,7 @@ async def solo(ctx: SlashContext, character, request=""):
         "model":model,
         "messages":messages,
         "temperature":0.8,
-        "max_tokens":150}
+        "max_tokens":200}
     # payload = {"model":"text-davinci-003","prompt":prompt,"temperature":0.8,"max_tokens":150}
     # payload = {"model":model,"prompt":prompt,"temperature":0.8,"max_tokens":150}
     payload = json.dumps(payload, indent = 4)
@@ -66,6 +67,15 @@ async def solo(ctx: SlashContext, character, request=""):
     description += f"\n\n{r.json()['choices'][0]['message']['content']}"
     embed = Embed(title=f"Here is your solo prompt!", description=description)
     embed.set_footer(text=footer)
+    await ctx.send(embed=embed)
+
+@slash.slash(name="help", description="Get help with the bot.")
+async def help(ctx: SlashContext):  
+    await ctx.defer()
+    title = "AI Suggestions Help"
+    description = "This bot generates scene ideas based on brief character descriptions you supply. It uses the OpenAI API to generate text, and the Discord API to send it to you. The bot is currently in beta, so please report any bugs to the developer, @lxgrf."
+    description += f"\n\nThe bot is currently using the {model} model."
+    embed = Embed(title=title, description=description)
     await ctx.send(embed=embed)
 
 bot.run(discord_api)
